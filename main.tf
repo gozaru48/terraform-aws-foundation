6345ee62500e42fe2f96f9ce25c5bc984eca7b68
Yusuke <yskozai1116@gmail.com>
2026-09-16 18:28:42 +0800

    Fix EC2 instance type formatting

--- main.tf
+++ main.tf
@@ -84,9 +84,7 @@ data "aws_ami" "amazon_linux" {
 
 resource "aws_instance" "web" {
   ami                         = data.aws_ami.amazon_linux.id
-  instance_type               = "t3
-  
-  .micro"
+  instance_type               = "t3.micro"
   subnet_id                   = aws_subnet.public.id
   vpc_security_group_ids      = [aws_security_group.web.id]
   associate_public_ip_address = true
